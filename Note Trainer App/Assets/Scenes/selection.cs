69adfd50790f094e2b89c07de79b5b051084935e
Sofia Bzhilyanskaya <sab4633@rit.edu>
2020-07-25 22:23:02 -0400

    Configured C4 Text
    Set up everything for C4 Note in regards to selection and playing

---
+++ Note Trainer App/Assets/Scenes/selection.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems; // 1
+using UnityEngine.UI;
+
+public class selection : MonoBehaviour, IPointerClickHandler // 2
+  
+// ... And many more available!
+{
+    public AudioSource C4Audio;
+    private bool selected = false;
+
+
+    public void OnPointerClick(PointerEventData eventData) // 3
+    {
+
+        C4Audio.Play();
+        if (selected)
+        {
+            this.GetComponent<Text>().color = Color.black;
+        }
+        else
+        {
+            this.GetComponent<Text>().color = Color.red;
+        }
+        selected = !selected;
+        
+    }
+}
