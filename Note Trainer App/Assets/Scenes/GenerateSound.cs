1e83d2234c071e5715d18dca50a6a03cb35be41b
Sofia Bzhilyanskaya <sab4633@rit.edu>
2020-07-27 11:23:26 -0400

    Randomly Generates Sound
    Added listener to allow a randomly generate sound to play on click

--- Note Trainer App/Assets/Scenes/GenerateSound.cs
+++ Note Trainer App/Assets/Scenes/GenerateSound.cs
@@ -3,18 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(Button))]
+
 public class GenerateSound : MonoBehaviour
 {
     //public int elements;
     public AudioSource[] notes;
     private int random_note;
 
+    private Button button { get { return GetComponent<Button>(); } }
+
     // Start is called before the first frame update
     void Start()
     {
+        gameObject.AddComponent<AudioSource>();
         Debug.Log("starting");
         random_note = 0;
+
+        button.onClick.AddListener(() => playSound());
     }
+
+    void playSound()
+    {
+        random_note = (int)Random.Range(0, 8);
+        notes[random_note].Play();
+    }
+    /*
 
     void OnMouseDown()
     {
@@ -22,7 +36,7 @@ public class GenerateSound : MonoBehaviour
         random_note = (int)Random.Range(0, 8);
         notes[random_note].Play();
     }
-
+    */
     /*
     void PlayRandomNote()
     {
