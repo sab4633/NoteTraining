1986c85707b70919f13b52b4b081503e524b57db
Sofia Bzhilyanskaya <sab4633@rit.edu>
2020-07-26 21:33:11 -0400

    Found issue with OnMouseDown
    (unsolved) investigate the event handler and button onclick more at a later date

---
+++ Note Trainer App/Assets/Scenes/GenerateSound.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GenerateSound : MonoBehaviour
+{
+    //public int elements;
+    public AudioSource[] notes;
+    private int random_note;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("starting");
+        random_note = 0;
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("trying something");
+        random_note = (int)Random.Range(0, 8);
+        notes[random_note].Play();
+    }
+
+    /*
+    void PlayRandomNote()
+    {
+        random_note = (int)Random.Range(0, 8);
+        notes[random_note].Play();
+    }
+    */
+}
